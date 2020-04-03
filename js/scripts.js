ba632f80b1cfd23a0eae8884ff93a27ca9fd8293
Sean Downs <spdowns@protonmail.com>
2020-04-03 08:19:58 -0700

    creates variables and functions that will hopefully identify if the number contains the interger 1 2 or 3 and will insert the replacement on the return.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 // Back End
+var tiggerNumber = [1, 2, 3]
+var replacments = ["beep", "boop", "Won't you be my neighbor?"]
 
 
+ var finalNumber = function(firstNumber) {
+  var inputNumber = firstNumber.split("")
+  var outputNumber = inputNumber.map(function())
 
+ }
 
 // Front End
 
 $(document).ready(function(){
-
-  $(form).submit(function(event){
+  $("form#input").submit(function(event){
     event.preventDefault();
-  });
+  
+    var firstNumber = parseint($("input#input").val());
+   
     $(".output").html();
 });
